40d372a8964d27ce74ce5798dedf87f85fc527dd
Brandon <brandon_scott585@yahoo.com>
2022-07-22 08:23:02 -0700

    adding header

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL
+#define SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+
+
+
+
+
+
+
+#endif
