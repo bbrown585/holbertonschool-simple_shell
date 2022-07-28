561f6ac6a74c21aaa09d1c654a23aab171c88b18
Brandon <brandon_scott585@yahoo.com>
2022-07-28 16:01:12 -0700

    updating

--- array_var.c
+++ array_var.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * str_tok - string token
+ * @str: string
+ * Return: token
+ */
 char **str_tok(char *str)
 {
 	static char **tokens;
 	int i = 0;
 	char *buffer = strdup(str);
 	char *token;
-	int len = 0; numtokens = 0;
+	int len = 0;
+	int numtokens = 0;
 
 	while (buffer[len])
 	{
@@ -25,13 +31,7 @@ char **str_tok(char *str)
 		token = strtok(NULL, " ");
 		tokens[i] = token;
 		i++;
-	}
-	
-	return (tokens);
 }
 
-int main(void)
-{
-	char **array;
-
-
+	return (tokens);
+}
